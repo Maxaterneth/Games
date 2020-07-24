90da6e73e584b0db04dff5c9d6a6d2b507e9bc14
Maxi <maximora213@gmail.com>
2020-07-23 23:24:16 -0400

    borrar

---
+++ calculadora.py
@@ -0,0 +1,377 @@
+import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            
+            
+            
+            import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+ 
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        input_number = int(input("number: "))
+        if input_number == random_number:
+            print(f"ganaste el numero es {input_number}")
+
+
+
+
+            
+        
+
+if __name__ == "__main__":
+    game()
+            
+        
+
+if __name__ == "__main__":
+    game()
