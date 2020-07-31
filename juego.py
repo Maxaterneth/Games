6c396edc5046c0a28c68d13ca59180e38e6957f1
Maxi <maximora213@gmail.com>
2020-07-31 19:26:31 -0400

    version 1

---
+++ juego.py
@@ -0,0 +1,29 @@
+import random
+
+def game():
+    random_number = random.randint(1, 100)
+    vidas = int(input("cuantas vidas quieres: "))
+    input_number = int(input("number: "))
+
+
+    while input_number != random_number:
+
+        if input_number < random_number:
+            print("Es mas grande")
+            vidas -= 1
+        else:
+            print("Es mas pequeño")
+            vidas -= 1
+
+        if vidas == 0:
+            print(f"perdiste el numero era {random_number}")
+            break
+        
+        input_number = int(input("number: "))
+
+
+    if input_number == random_number:
+        print(f"ganaste el numero es {random_number}")
+
+if __name__ == "__main__":
+    game()
